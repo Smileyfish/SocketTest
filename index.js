677d9b901f65cd7322256507a8fb327c97a29b6d
Smileyfish <masal5330@hotmail.com>
2025-02-17 11:26:26 +0100

    Refactor private chat functionality only in one room rn and improve socket connection handling

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+/*
 if (cluster.isPrimary) {
   const numCPUs = availableParallelism();
   // create one worker per available core
@@ -25,84 +25,88 @@ if (cluster.isPrimary) {
 
   // set up the adapter on the primary thread
   setupPrimary();
-} else {
-  const db = await setupDatabase();
+} else { */
+const db = await setupDatabase();
 
-  const app = express();
-  const server = createServer(app);
-  const io = new Server(server, {
-    connectionStateRecovery: {},
-    adapter: createAdapter(),
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+  pingInterval: 5000, // Ping every 5 seconds (default is 25s)
+  pingTimeout: 10000, // Ping Timeout 10 seconds (default is 60)
+  // adapter: createAdapter(),
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+// Route to serve the registration page
+app.get("/register", (req, res) => {
+  res.sendFile(join(__dirname, "views/register.html"));
+});
+
+// Route to serve the login page
+app.get("/login", (req, res) => {
+  res.sendFile(join(__dirname, "views/login.html"));
+});
+
+app.get("/protected", authenticateJWT, (req, res) => {
+  res.send("<h1>This is a protected route</h1>");
+});
+
+//Protect the chat routes
+app.use(authenticateJWT);
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (!token) return next(new Error("Authentication error"));
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(new Error("Authentication error"));
+    socket.user = user;
+    next();
   });
+});
 
-  const __dirname = dirname(fileURLToPath(import.meta.url));
+io.on("connection", async (socket) => {
+  console.log("a user connected", socket.user.username);
 
-  app.use(express.json());
+  // Emit the user information to the client
+  socket.emit("user info", socket.user);
 
-  app.use("/api/users", userRoutes);
-
-  app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "index.html"));
-  });
-
-  // Route to serve the registration page
-  app.get("/register", (req, res) => {
-    res.sendFile(join(__dirname, "views/register.html"));
-  });
-
-  // Route to serve the login page
-  app.get("/login", (req, res) => {
-    res.sendFile(join(__dirname, "views/login.html"));
-  });
-
-  app.get("/protected", authenticateJWT, (req, res) => {
-    res.send("<h1>This is a protected route</h1>");
-  });
-
-  //Protect the chat routes
-  app.use(authenticateJWT);
-
-  io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    if (!token) return next(new Error("Authentication error"));
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) return next(new Error("Authentication error"));
-      socket.user = user;
-      next();
-    });
-  });
-
-  io.on("connection", async (socket) => {
-    console.log("a user connected", socket.user.username);
-
-    // Emit the user information to the client
-    socket.emit("user info", socket.user);
-
-    socket.on("chat message", async (msg, clientOffset, callback) => {
-      let result;
-      try {
-        // store the message in the database
-        result = await db.run(
-          "INSERT INTO messages (content, client_offset) VALUES (?,?)",
-          msg,
-          clientOffset
-        );
-      } catch (e) {
-        if (e.errno === 19) {
-          callback();
-        } else {
-        }
-        return;
+  socket.on("chat message", async (msg, clientOffset, callback) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run(
+        "INSERT INTO messages (content, client_offset) VALUES (?,?)",
+        msg,
+        clientOffset
+      );
+    } catch (e) {
+      if (e.errno === 19) {
+        callback();
+      } else {
       }
-      // include the offset with the message
-      io.emit("chat message", msg, result.lastID);
-      console.log("message: " + msg);
-      callback();
-    });
+      return;
+    }
+    // include the offset with the message
+    io.emit("chat message", msg, result.lastID);
+    console.log("message: " + msg);
+    callback();
+  });
 
-    socket.on("private message", async ({ to, message }, callback) => {
-      try {
+  socket.on("private chat message", async (message, callback) => {
+    io.emit("private chat message", message);
+
+    /*try {
         const chatRoomId = await getOrCreateChatRoom(
           db,
           socket.user.username,
@@ -114,44 +118,44 @@ if (cluster.isPrimary) {
           "INSERT INTO chat_messages (chat_room_id, sender_id, content) VALUES (?, ?, ?)",
           [chatRoomId, socket.user.id, message]
         );
-
-        callback({ success: true, messageId: result.lastID });
-      } catch (error) {
-        callback({ success: false, error: error.message });
-      }
-    });
-
-    socket.on("join private chat", async ({ to }) => {
-      const chatRoomId = await getOrCreateChatRoom(
-        db,
-        socket.user.username,
-        to
-      );
-      socket.join(`chat_${chatRoomId}`);
-    });
-
-    if (!socket.recovered) {
-      // if the connection state recovery was not successful
-      try {
-        await db.each(
-          "SELECT id, content FROM messages WHERE id > ?",
-          [socket.handshake.auth.serverOffset || 0],
-          (_err, row) => {
-            socket.emit("chat message", row.content, row.id);
-          }
-        );
-      } catch (e) {
-        // something went wrong
-      }
-    }
-
-    socket.on("disconnect", () => {
-      console.log("user disconnected", socket.user.username);
-    });
+      } catch (error) {}*/
+    callback();
   });
 
-  const port = process.env.PORT;
+  socket.on("join private chat", async ({ to }, callback) => {
+    const chatRoomId = await getOrCreateChatRoom(db, socket.user.username, to);
+    socket.join(`chat_${chatRoomId}`);
+    console.log(`user ${socket.user.username} joined chat_${chatRoomId}`);
+    callback();
+  });
 
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.user.username);
+  });
+});
+
+const port = process.env.PORT;
+
+server.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
+
+/*
   server.listen(port, () => {
     const host = process.env.RENDER_EXTERNAL_URL || "localhost";
     const url = process.env.RENDER_EXTERNAL_URL
@@ -159,7 +163,8 @@ if (cluster.isPrimary) {
       : `http://${host}:${port}`;
     console.log(`server running at ${url}`);
   });
-}
+
+}*/
 
 async function getOrCreateChatRoom(db, user1, user2) {
   // Ensure user1_id is always smaller than user2_id to avoid duplicate entries
